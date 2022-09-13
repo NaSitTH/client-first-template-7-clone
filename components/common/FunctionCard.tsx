8a9eeb583494359320879e48d028d5eaf2217ba8
NaSitTH <inhacfc@hotmail.com>
2022-09-13 17:22:46 +0700

    add FunctionCard.tsx

---
+++ components/common/FunctionCard.tsx
@@ -0,0 +1,25 @@
+import { IconContext } from "react-icons";
+
+type AppProps = {
+  title: string;
+  text: string;
+  icon: JSX.Element;
+};
+
+const FunctionCard = ({ title, text, icon }: AppProps) => {
+  return (
+    <IconContext.Provider value={{ className: "text-[#DDFFDF] w-4 h-4" }}>
+      <div className="flex space-x-6">
+        <div className="flex h-7 w-7 shrink-0 items-center justify-center rounded bg-[#83D187]">
+          {icon}
+        </div>
+        <div className="">
+          <p className="text-2xl font-bold leading-[1.76rem]">{title}</p>
+          <p className="mt-2 text-app-secondary-text">{text}</p>
+        </div>
+      </div>
+    </IconContext.Provider>
+  );
+};
+
+export default FunctionCard;
--- components/common/index.js
+++ components/common/index.js
@@ -1,2 +1,3 @@
 export { default as ButtonLink } from "./ButtonLink";
 export { default as TitleSection } from "./TitleSection";
+export { default as FunctionCard } from "./FunctionCard";

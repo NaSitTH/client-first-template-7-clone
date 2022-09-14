4e38939b14024f7199009fe4a90f84eccbb83d8c
NaSitTH <inhacfc@hotmail.com>
2022-09-14 07:22:25 +0700

    add ProjectRow.tsx

---
+++ components/common/ProjectCard.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import ButtonLink from "./ButtonLink";
+
+type AppProps = {
+  title: string;
+  text: string;
+  image: string;
+  url: string;
+};
+
+const ProjectCard = ({ title, text, image, url }: AppProps) => {
+  return (
+    <div className="relative h-full w-full overflow-hidden rounded-xl">
+      <Image
+        src={image}
+        alt={`${title} image`}
+        layout="fill"
+        objectFit="cover"
+      />
+      <div className="h-full w-full bg-app-black opacity-50"></div>
+      <div className="absolute top-0 left-0 flex h-full w-full flex-col justify-between p-12 text-white">
+        <div className="mt-8">
+          <h3>{title}</h3>
+          <p className="mt-4">{text}</p>
+        </div>
+        <div className="grow-0">
+          <ButtonLink url={url} title="See more" />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ProjectCard;
--- components/common/index.js
+++ components/common/index.js
@@ -1,3 +1,6 @@
 export { default as ButtonLink } from "./ButtonLink";
 export { default as TitleSection } from "./TitleSection";
 export { default as FunctionCard } from "./FunctionCard";
+export { default as LogoRow } from "./LogoRow";
+export { default as ProjectCard } from "./ProjectCard";
+export { default as ProjectRow } from "./ProjectRow";

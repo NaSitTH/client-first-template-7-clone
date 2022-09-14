945d08e2271ca43427df2f29df5a0e734c4e0e35
NaSitTH <inhacfc@hotmail.com>
2022-09-14 22:10:52 +0700

    add margin top

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import { footerUrl } from "../../data/Url";
 
 const Footer = () => {
   return (
-    <footer className="z-30 w-screen bg-app-black py-24 text-white xl:py-20 lg:py-16">
+    <footer className="app-section-mt z-30 w-screen bg-app-black py-24 text-white xl:py-20 lg:py-16">
       <div className="app-container flex justify-between lg:grid lg:grid-cols-3 lg:gap-y-16 sm:grid-cols-1 sm:gap-y-10">
         <h4 className="col-span-1 w-24">Logo</h4>
         <div className="flex space-x-20 xl:space-x-14 lg:col-span-2 lg:space-x-20 md:justify-between md:space-x-0 sm:col-span-1">

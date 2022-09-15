32ddf2bf64e74f660715c8b2a77839f4f43dd600
NaSitTH <inhacfc@hotmail.com>
2022-09-15 17:52:55 +0700

    add about-us.tsx

---
+++ pages/about-us.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from "next";
+import { AboutHeaderSection } from "../components/about";
+
+const AboutUs:NextPage = () => {
+  return (
+    <>
+    {/* Header */}
+    <AboutHeaderSection/>
+    
+    {/* Our mission */}
+
+    {/* Awards */}
+
+    {/* Our journey */}
+
+    {/* Our team */}
+
+    {/* CTA */}
+
+    {/* Blogs */}
+
+    {/* Events */}
+    
+    
+    </>
+
+  )
+}
+
+export default AboutUs

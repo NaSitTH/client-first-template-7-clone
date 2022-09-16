60ad383659798082a521ea06c3141e945ebf91a8
NaSitTH <inhacfc@hotmail.com>
2022-09-16 07:26:22 +0700

    add MemberCard.tsx

--- components/common/card/index.js
+++ components/common/card/index.js
@@ -3,3 +3,4 @@ export { default as EventCard } from "./EventCard";
 export { default as FunctionCard } from "./FunctionCard";
 export { default as ProjectCard } from "./ProjectCard";
 export { default as StatsCard } from "./StatsCard";
+export { default as MemberCard } from "./MemberCard";

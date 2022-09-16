4fd76cb530a51c1617969702137f9ac218ba429e
NaSitTH <inhacfc@hotmail.com>
2022-09-16 07:24:55 +0700

    add AboutHeaderSection.tsx

--- components/about/AboutHeaderSection.tsx
+++ components/about/AboutHeaderSection.tsx
@@ -1,18 +1,26 @@
-import { TitleSection } from "../common/section"
+import { TitleSection } from "../common/section";
 
 const AboutHeaderSection = () => {
   return (
     <section className="app-container app-section-mt text-app-primary-text">
-        <TitleSection title="KNOW ABOUT US"/>
-        <div className="flex w-full mt-8">
-            <h1 className="w-full ml-24 mr-16">We are a nonprofit team working worldwide</h1>
-            <div className="w-2/3">
-                <p className="text-xl leading-[1.875rem] font-bold">Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
-                <p className="mt-4 opacity-70">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Suspendisse varius enim elementum tristique.</p>
-            </div>
+      <TitleSection title="KNOW ABOUT US" />
+      <div className="mt-8 ml-24 flex">
+        <h1 className="w-full">We are a nonprofit team working worldwide</h1>
+        <div className="w-[72%]">
+          <p className="text-xl font-bold leading-[1.875rem]">
+            Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc
+            ut sem vitae risus tristique posuere.
+          </p>
+          <p className="mt-4 opacity-70">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim in eros elementum tristique. Duis cursus, mi quis
+            viverra ornare, eros dolor interdum nulla, ut commodo diam libero
+            vitae erat. Suspendisse varius enim elementum tristique.
+          </p>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default AboutHeaderSection
+export default AboutHeaderSection;

a7409d1ebef3d7833eb3b036b32b893b5b3f28e1
NaSitTH <inhacfc@hotmail.com>
2022-09-16 07:26:44 +0700

    add AboutAwardsSection.tsx

---
+++ components/about/AboutAwardsSection.tsx
@@ -0,0 +1,8 @@
+const AboutAwardsSection = () => {
+  return (
+    <section className="app-container app-section-mt">
+      <div className="flex justify-between px-[90px]"></div>
+    </section>
+  );
+};
+export default AboutAwardsSection;

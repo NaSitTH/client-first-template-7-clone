59cee34b7e6e769ac5ec4d864def33804330aa08
NaSitTH <inhacfc@hotmail.com>
2022-09-17 11:28:32 +0700

    add dynamic row-col

--- components/common/card/BlogCard.tsx
+++ components/common/card/BlogCard.tsx
@@ -7,21 +7,28 @@ type AppProps = {
   text: string;
   image: string;
   url: string;
+  isRow?: boolean;
 };
 
-const BlogCard = ({ title, text, image, url }: AppProps) => {
+const BlogCard = ({ title, text, image, url, isRow }: AppProps) => {
   return (
     <Link href={url}>
-      <a className="group cursor-pointer">
+      <a
+        className={`group flex cursor-pointer ${
+          isRow ? "flex-row items-center space-x-6" : "flex-col"
+        }`}
+      >
         <div className="relative h-[192px] w-full overflow-hidden rounded-lg transition group-hover:scale-105">
           <Image src={image} alt={title} layout="fill" objectFit="cover" />
         </div>
-        <p className="mt-4 w-full text-xl font-medium leading-[1.875rem] text-[#1C1D28]">
-          {title}
-        </p>
-        <p className="mt-2 text-app-secondary-text line-clamp-3 group-hover:line-clamp-none">
-          {text}
-        </p>
+        <div className={isRow ? "mt-0" : "mt-4"}>
+          <p className="w-full text-xl font-medium leading-[1.875rem] text-[#1C1D28]">
+            {title}
+          </p>
+          <p className="mt-2 text-app-secondary-text line-clamp-3 group-hover:line-clamp-none">
+            {text}
+          </p>
+        </div>
       </a>
     </Link>
   );

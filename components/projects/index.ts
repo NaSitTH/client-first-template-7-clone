99e8fab70591ac75b18832bc53ed786813db0845
NaSitTH <inhacfc@hotmail.com>
2022-09-19 09:02:30 +0700

    change index.js to index.ts

---
+++ components/projects/index.ts
@@ -0,0 +1,3 @@
+export { default as ProjectsHeaderSection } from "./ProjectsHeaderSection";
+export { default as ProjectsWhatWeDoSection } from "./ProjectsWhatWeDoSection";
+export { default as ProjectsProjectSection } from "./ProjectsProjectSection";
--- components/services/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ServicesHeaderSection } from "./ServicesHeaderSection";
-export { default as ServicesWhatWeDoSection } from "./ServicesWhatWeDoSection";
-export { default as ServicesProjectSection } from "./ServicesProjectSection";

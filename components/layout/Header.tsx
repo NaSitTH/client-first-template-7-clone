e8b435409262def2a5e0ff00ffdd6b9fd902dba8
NaSitTH <inhacfc@hotmail.com>
2022-09-25 12:11:28 +0700

    update change bg-color by page

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -5,9 +5,26 @@ import NavLink from "./NavLink";
 import { FaBars } from "react-icons/fa";
 import { Backdrop, NavModal } from "../modal";
 import { navUrl } from "../../lib/constant";
+import { useRouter } from "next/router";
 
 const Header = () => {
   const [navIsOpen, setNavOpen] = useState(false);
+  const router = useRouter();
+  const path = router.asPath;
+
+  let bg: string = "bg-white";
+
+  if (
+    path.includes("/media") ||
+    path.includes("/donation") ||
+    path.includes("/event")
+  ) {
+    bg = "bg-app-green-tertiary";
+  } else if (path.includes("/contact")) {
+    bg = "bg-app-off-white";
+  } else {
+    bg = "bg-white";
+  }
 
   const toggleNavHandler = () => {
     setNavOpen(!navIsOpen);
@@ -18,39 +35,41 @@ const Header = () => {
   };
 
   return (
-    <header className="app-container z-30 flex h-[67px] items-center justify-between">
-      <Link href={navUrl.home}>
-        <a className="flex h-full items-center">
-          <h4>Logo</h4>
-        </a>
-      </Link>
-      <ul className="flex items-center space-x-10 text-button font-medium lg:space-x-5 md:hidden">
-        <li>
-          <NavLink url="/" title="Home" />
-        </li>
-        <li>
-          <NavLink url={navUrl.aboutUs} title="About us" />
-        </li>
-        <li>
-          <NavLink url={navUrl.whatWeDo} title="What we do" />
-        </li>
-        <li>
-          <NavLink url={navUrl.media} title="Media" />
-        </li>
-        <li>
-          <NavLink url={navUrl.contact} title="Contact" />
-        </li>
-        <li className="flex items-center rounded bg-app-primary-text text-white lg:px-6">
-          <Link href={navUrl.donate}>
-            <a className="px-8 py-4">Donate</a>
-          </Link>
-        </li>
-      </ul>
-      <button className="hidden md:block" onClick={toggleNavHandler}>
-        <FaBars className="h-6 w-6 text-app-primary-text" />
-      </button>
-      <NavModal handleClick={closeNavHandler} isOpen={navIsOpen} />
-      {navIsOpen && <Backdrop handleClick={closeNavHandler} />}
+    <header className={`w-screen ${bg} `}>
+      <nav className="app-container z-30 flex h-[67px] items-center justify-between">
+        <Link href={navUrl.home}>
+          <a className="flex h-full items-center">
+            <h4>Logo</h4>
+          </a>
+        </Link>
+        <ul className="flex items-center space-x-10 text-button font-medium lg:space-x-5 md:hidden">
+          <li>
+            <NavLink url="/" title="Home" />
+          </li>
+          <li>
+            <NavLink url={navUrl.aboutUs} title="About us" />
+          </li>
+          <li>
+            <NavLink url={navUrl.whatWeDo} title="What we do" />
+          </li>
+          <li>
+            <NavLink url={navUrl.media} title="Media" />
+          </li>
+          <li>
+            <NavLink url={navUrl.contact} title="Contact" />
+          </li>
+          <li className="flex items-center rounded bg-app-primary-text text-white lg:px-6">
+            <Link href={navUrl.donate}>
+              <a className="px-8 py-4">Donate</a>
+            </Link>
+          </li>
+        </ul>
+        <button className="hidden md:block" onClick={toggleNavHandler}>
+          <FaBars className="h-6 w-6 text-app-primary-text" />
+        </button>
+        <NavModal handleClick={closeNavHandler} isOpen={navIsOpen} />
+        {navIsOpen && <Backdrop handleClick={closeNavHandler} />}
+      </nav>
     </header>
   );
 };

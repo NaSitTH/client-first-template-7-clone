16fdff8ab0dd4b1e520f3b8c8a8c0c19dcd53039
NaSitTH <inhacfc@hotmail.com>
2022-09-25 11:25:04 +0700

    update style

--- components/common/card/EventCard.tsx
+++ components/common/card/EventCard.tsx
@@ -1,26 +1,25 @@
 import Link from "next/link";
 import { BsArrowRight } from "react-icons/bs";
-
 import { format } from "date-fns";
 
 type Props = {
   title: string;
-  date: Date;
   url: string;
+  date: Date;
 };
 
 const EventCard = ({ title, date, url }: Props) => {
-  let month = format(date, "MMM");
-  let year = format(date, "yy");
+  const day = format(date, "dd");
+  const month = format(date, "MMM");
 
   return (
     <Link href={url}>
-      <a className="flex w-full justify-between space-x-6 rounded-lg bg-app-green-secondary pl-10 pr-12 text-app-primary-text">
-        <div className="mt-8">
-          <h2 className="font-medium">{year}</h2>
-          <p className="text-button tracking-[0.125em]">{month}</p>
+      <a className="flex h-48 w-full justify-between space-x-6 rounded-lg bg-app-green-secondary pl-10 pr-12 text-app-primary-text">
+        <div className="mt-8 grow-0">
+          <h2 className="font-medium">{day}</h2>
+          <p className="text-button font-medium tracking-[0.125em]">{month}</p>
         </div>
-        <div className="my-12">
+        <div className="my-12 grow">
           <div className="flex items-center space-x-6">
             <p className="text-button font-medium tracking-[0.125em]">
               NEXT EVENTS
@@ -29,7 +28,7 @@ const EventCard = ({ title, date, url }: Props) => {
               <span className="h-0.5 w-[44px] bg-app-primary-text"></span>
             </span>
           </div>
-          <h3 className="mt-2">{title}</h3>
+          <h3 className="mt-2 line-clamp-2">{title}</h3>
         </div>
         <div className="mt-16 flex h-14 w-14 shrink-0 items-center justify-center rounded-full bg-white">
           <BsArrowRight className="h-7 w-7" />
